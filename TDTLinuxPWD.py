954e53047dcb2f353e461f4580c60cd2c7d11499
Willy Stadnick <willy.stadnick@gmail.com>
2022-12-07 01:09:25 -0300

    Add font format helper

--- TDTLinuxPWD.py
+++ TDTLinuxPWD.py
@@ -12,30 +12,71 @@ from argparse import ArgumentParser
 
 os.system("clear")
 
-#Color scheme
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+#Font format
+class ff:
+    END = '\033[0m'
     BOLD = '\033[1m'
+    THIN = '\033[2m'
+    ITALIC = '\033[3m'
     UNDERLINE = '\033[4m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+
+
+    def bold(text):
+        return ff.BOLD + str(text) + ff.END
+
+
+    def thin(text):
+        return ff.THIN + str(text) + ff.END
+
+
+    def italic(text):
+        return ff.ITALIC + str(text) + ff.END
+
+
+    def underline(text):
+        return ff.UNDERLINE + str(text) + ff.END
+
+
+    def red(text):
+        return ff.RED + str(text) + ff.END
+
+
+    def green(text):
+        return ff.GREEN + str(text) + ff.END
+
+
+    def yellow(text):
+        return ff.YELLOW + str(text) + ff.END
+
+
+    def blue(text):
+        return ff.BLUE + str(text) + ff.END
+
+
+    def purple(text):
+        return ff.PURPLE + str(text) + ff.END
+
 
 #Create Banner
 def banner():
-    print("""%s
+    print("""
+    %s
       _____   ____    _____   _       _                          ____   __        __  ____
      |_   _| |  _ \  |_   _| | |     (_)  _ __    _   _  __  __ |  _ \  \ \      / / |  _ \\
        | |   | | | |   | |   | |     | | | '_ \  | | | | \ \/ / | |_) |  \ \ /\ / /  | | | |
        | |   | |_| |   | |   | |___  | | | | | | | |_| |  >  <  |  __/    \ V  V /   | |_| |
-       |_|   |____/    |_|   |_____| |_| |_| |_|  \__,_| /_/\_\ |_|        \_/\_/    |____/ %s%s
-
+       |_|   |____/    |_|   |_____| |_| |_| |_|  \__,_| /_/\_\ |_|        \_/\_/    |____/
+    %s
+    %s
      # Coded By Tiago Neves
      # Github https://github.com/TiagoANeves
-
-    """ % (bcolors.OKBLUE, bcolors.ENDC, bcolors.FAIL))
+    %s
+    """ % (ff.BLUE, ff.END, ff.RED, ff.END))
 
 # Count wordlist password
 def countlines(textfile):
@@ -48,7 +89,7 @@ if __name__ == "__main__":
         banner()
         print("This program will check the users in the shadow file and use a wordlist to try crack the password hashes.")
         print("If wordlist not especified, it will use the wordlist.txt by default.")
-        print(bcolors.ENDC)
+        print()
         parser = ArgumentParser()
         parser.add_argument("-s", "--shadow", help="shadow file")
         parser.add_argument("-w", "--wordlist", default="wordlist.txt", help="wordlist file")
@@ -56,17 +97,17 @@ if __name__ == "__main__":
     except:
         sys.exit(0)
     if len(sys.argv) < 3:
-        print(bcolors.WARNING + "Usage: python "+sys.argv[0]+" -s shadown -w wordlist.txt\n" + bcolors.ENDC)
-        print(bcolors.WARNING + "Use "+sys.argv[0]+" -h or --help to print the help option\n" + bcolors.ENDC)
+        print(ff.yellow("Usage: python "+sys.argv[0]+" -s shadown -w wordlist.txt"))
+        print(ff.yellow("Use "+sys.argv[0]+" -h or --help to print the help option"))
         sys.exit()
     else:
-        print(bcolors.HEADER + "Starting the program...\n" + bcolors.ENDC)
+        print(ff.purple("Starting the program..."))
         numlines = countlines(args.wordlist)
-        print(bcolors.HEADER + "The file %s%s%s has %s%s%s passwords" %(bcolors.OKGREEN,args.wordlist,bcolors.HEADER,bcolors.OKGREEN,numlines,bcolors.HEADER) + bcolors.ENDC	)
+        print(ff.purple("The file ") + ff.green(args.wordlist) + ff.purple(" has ") + ff.green(numlines) + ff.purple(" passwords"))
         try:
             fileshadow = open(args.shadow,'r')
         except:
-            print("Error trying to open shadow file")
+            print(ff.red("Error trying to open shadow file"))
             sys.exit()
         users = fileshadow.read().split('\n')
         for user in users:
@@ -75,23 +116,23 @@ if __name__ == "__main__":
                 hashes = userline[1].split("$")
                 salt = "$"+hashes[1]+"$"+hashes[2]+"$"
                 passfound = 0
-                print(bcolors.WARNING + "\nBruteforcing password for the user %s%s" %(bcolors.OKGREEN,userline[0]) + bcolors.ENDC)
+                print(ff.yellow("Cracking password for the user ") + ff.green(userline[0]))
                 try:
                     filewordlist = open(args.wordlist,'r')
                 except:
-                    print("Error trying to open wordlist file")
+                    print(ff.red("Error trying to open wordlist file"))
                     sys.exit()
                 passwords = filewordlist.read().split('\n')
                 pwdcount = 0
                 for password in passwords:
                     try:
                         pwdcount += 1
-                        print(bcolors.WARNING + "Trying %s%s%s of %s%s%s passwords" %(bcolors.OKGREEN,pwdcount,bcolors.WARNING,bcolors.OKGREEN,numlines,bcolors.WARNING) + bcolors.ENDC)
+                        print(ff.yellow("Trying ") + ff.green(pwdcount) + ff.yellow(" of ") + ff.green(numlines) + ff.yellow(" passwords"))
                         result = crypt.crypt(password, salt)
                         if (result == userline[1]):
                             sys.stdout.write("\r")
                             sys.stdout.flush()
-                            print(bcolors.OKBLUE + bcolors.BOLD + bcolors.UNDERLINE + "Password found: " + bcolors.OKGREEN + password + bcolors.ENDC)
+                            print(ff.underline(ff.blue("Password found:")) + " " + ff.bold(ff.green(password)))
                             passfound = 1
                             break
                         else:
@@ -100,7 +141,7 @@ if __name__ == "__main__":
                             sys.stdout.write("\r")
                             sys.stdout.flush()
                     except KeyboardInterrupt:
-                        print(bcolors.FAIL + "Bruteforcing interrupted by the user..." + bcolors.ENDC)
+                        print(ff.red("Cracking aborted."))
                         break
-                        if (passfound == 0):
-                            print(bcolors.FAIL + "Could not find the password for the user "+userline[0] + bcolors.ENDC)
+                if (passfound == 0):
+                    print(ff.red("Could not crack the user's password"))
